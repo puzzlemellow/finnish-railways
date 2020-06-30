605802623992d99931ef7c9cd633dce8533607be
puzzlemellow <elmeri.kemppainen@gmail.com>
2020-06-30 13:23:31 +0300

    Search bar struct & placeholders

--- train-app/views/SearchView.swift
+++ train-app/views/SearchView.swift
@@ -1,8 +1,67 @@
 import SwiftUI
 
 struct SearchView: View {
+    /*
+     Searchfield bindings
+     */
+    @State private var depSearchValue: String = ""
+    @State private var desSearchValue: String = ""
+    @State private var activeDepartingSearch: Bool = false
+    @State private var activeDestinationSearch: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            self.departingSearch
+                .padding()
+            self.destinationSearch
+                .padding()
+                .offset(y: 70)
+        }
+    }
+}
+
+private extension SearchView {
+    var departingSearch: some View {
+        SearchBarView(
+            hint: "Choose Departing Station",
+            searchValue: self.$depSearchValue,
+            activity: self.$activeDepartingSearch)
+    }
+    
+    var destinationSearch: some View {
+        SearchBarView(
+            hint: "Choose Destination",
+            searchValue: self.$desSearchValue,
+            activity: self.$activeDestinationSearch)
+    }
+}
+
+struct SearchBarView: View {
+    let hint: String
+    @Binding var searchValue: String
+    @Binding var activity: Bool
+    
+    var body: some View {
+        VStack {
+            VStack {
+                HStack {
+                    TextField(self.hint, text: $searchValue, onEditingChanged: {
+                        typing in
+                        self.activity = true
+                    })
+                    Button(action: {
+                        self.searchValue = ""
+                        self.activity = false
+                    }) {
+                        Image(systemName: "xmark.circle.fill").opacity(self.searchValue.isEmpty ? 0 : 1)
+                    }
+                }
+                .padding()
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(5)
+            }
+            Spacer()
+        }
     }
 }
 
